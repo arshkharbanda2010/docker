6d17d686914d357af6ac8ce9520dc75e61632488
arshkharbanda2010 <arsh.kharbanda2010@gmail.com>
2022-06-18 17:11:13 +0530

    first commit

---
+++ app/main.py
@@ -0,0 +1,12 @@
+import imp
+from pymongo import MongoClient
+from pprint import PrettyPrinter, pprint
+
+MONGO_URL = "mongodb://mongo:27017"
+client = MongoClient(MONGO_URL)
+db = client.records
+col = db.links
+col.insert_one({"link":"google.com"})
+links = col.find({})
+for link in links:
+    pprint(link)
